1d11957c502b185d65a9936ceb6b1ae9eb55a27e
Marcel Transier <34482842+marceltransier@users.noreply.github.com>
2018-03-14 08:27:27 +0100

    Added files

---
+++ ttt_detectivesgift/lua/autorun/shared.lua
@@ -0,0 +1,44 @@
+--TODO: unterstützen der rolle reaper indem ich via convar regel welche rollen die waffe (wie der traitor) aufheben und anschauen können
+
+if SERVER then
+	AddCSLuaFile()
+	resource.AddFile("materials/vgui/ttt/icon_detectivesgift.png")
+end
+
+function AddDetectivesgiftToShop()
+  if (!EquipmentItems) then return end
+
+  EQUIP_DETECTIVESGIFT = GenerateNewEquipmentID()
+
+  local item = {
+    id = EQUIP_DETECTIVESGIFT,
+    type = "item_passive",
+    material = "vgui/ttt/icon_detectivesgift.png",
+    name = "Detective's Gift",
+    desc = "Buy this item before giving your\nweapons the detective.\nA look at the weapons you drop after\nbuying this item is deadly!"
+  }
+  table.insert(EquipmentItems[ROLE_TRAITOR], item)
+end
+
+function DetectivesgiftDrop(ply, wep)
+  if (!ply:HasEquipmentItem(EQUIP_DETECTIVESGIFT)) then return end
+  wep.detectivesgift = true
+end
+function DetectivesgiftPickup(ply, wep)
+  if (wep.detectivesgift == true && ply:GetRole() != 1) then return false end
+end
+
+function LookAtDetectivesgift ()
+  for i, ply in ipairs(team.GetPlayers(1)) do
+    if (ply:GetRole() != 1) then
+      if (ply:GetEyeTrace().Entity.detectivesgift == true) then
+        ply:Kill()
+      end
+    end
+	end
+end
+
+hook.Add("PlayerDroppedWeapon", "DetectivesgiftDrop", DetectivesgiftDrop)
+hook.Add("PlayerCanPickupWeapon", "DetectivesgiftPickup", DetectivesgiftPickup)
+hook.Add("Think", "LookAtDetectivesgift", LookAtDetectivesgift)
+hook.Add("Initialize", "AddDetectivesgiftToShop", AddDetectivesgiftToShop)
